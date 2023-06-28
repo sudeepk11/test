f64475574600690e3c10708771b9cfdf40dca066
sudeepk11 <79464046+sudeepk11@users.noreply.github.com>
2023-06-28 22:54:15 +0530

    INitial Push

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const el = screen.getByText(/hello world/i);
-  expect(el).toBeInTheDocument();
-});
