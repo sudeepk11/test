c1591d5955df4e106b94d45643506893e83aeaa2
sudeepk11 <79464046+sudeepk11@users.noreply.github.com>
2023-06-28 22:49:35 +0530

    API Folder

--- api/server.js
+++ api/server.js
@@ -1,5 +1,28 @@
-const port = parseInt(process.env.PORT || '3001', 10);
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
 
-require('./app').listen(port, () => {
-  console.log(`API listening at http://localhost:${port}`);
+// cors for cross origin stuff!!
+app.use(cors());
+
+// function to get imgs
+app.get('/api/flickr', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'http://api.flickr.com/services/feeds/photos_public.gne?format=json'
+    );
+    const jsonString = response.data.substring(15, response.data.length - 1);
+    const jsonData = JSON.parse(jsonString);
+    res.json(jsonData);
+  } catch (error) {
+    console.error('error!!', error);
+    res.status(500).json({ error: 'failed' });
+  }
 });
+
+// server start
+app.listen(4200, () => {
+  console.log('Server is running on port 4200');
+});
+
